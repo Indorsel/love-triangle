c7715e1eae67b20f4f23047bee3cc67815927bc9
Aliaksandr Yankouski <aleksandr.yankovskiy@gmail.com>
2018-02-20 17:23:29 +0300

    Update test.js
    update README.md
    
    lover-triangle-resolvation

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let length = preferences.length; 
+  let count = 0;  
+  for (let i = 0; i<length; i++) { 
+      let firstAngle = preferences[i] - 1;
+      if (firstAngle == i) {
+        continue
+      };
+      let secondAngle = preferences[firstAngle] - 1;
+      if (firstAngle == secondAngle) {
+        continue
+      };
+      let thirdAngle = preferences[secondAngle] - 1;
+      if (thirdAngle == secondAngle) {
+        continue
+      };
+      if (thirdAngle == i) {
+          count++;
+      }
+   }
+   return count / 3;
 };
